77ee260990109a9158547b16f279ffa939b4d50f
Sıtkı Bağdat <sbagdat@gmail.com>
2016-05-19 00:58:59 +0300

    feat: Integrate user authentication with proper functions

--- db_seed.py
+++ db_seed.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Base, Category, Item
+from database_setup import Base, Category, Item, User
 
 engine = create_engine('sqlite:///catalogapp.db')
 Base.metadata.bind = engine
@@ -11,39 +11,44 @@ session = DBSession()
 
 categories = [
     ['soccer',
-        [{'name':'two shinguards',
-           'description':'Description of two shinguards'},
-          {'name':'shinguards',
-           'description':'Description of singuards'},
-          {'name':'jersey',
-           'description':'Description of jersey'},
-          {'name':'soccer cleats',
-           'description':'Description of soccer cleats'}]],
+        [{'name': 'two shinguards',
+          'description': 'Description of two shinguards'},
+         {'name': 'shinguards',
+          'description': 'Description of singuards'},
+         {'name': 'jersey',
+          'description': 'Description of jersey'},
+         {'name': 'soccer cleats',
+          'description': 'Description of soccer cleats'}]],
     ['hockey',
-        [{'name':'stick',
-          'description':'Description of hockey stick'}]],
+        [{'name': 'stick',
+          'description': 'Description of hockey stick'}]],
     ['snowboarding',
-        [{'name':'goggles',
-          'description':'Description of goggles'},
-         {'name':'snowboard',
-          'description':'Description of snowboard'}]],
+        [{'name': 'goggles',
+          'description': 'Description of goggles'},
+         {'name': 'snowboard',
+          'description': 'Description of snowboard'}]],
     ['frisbee',
-        [{'name':'frisbee',
-          'description':'Description of frisbee'}]],
+        [{'name': 'frisbee',
+          'description': 'Description of frisbee'}]],
     ['baseball',
-        [{'name':'bat',
-          'description':'Description of bat'}]]
+        [{'name': 'bat',
+          'description': 'Description of bat'}]]
 ]
 
+current_user = User(name="System User", email="system@test.com")
+session.add(current_user)
+session.commit()
+
 for category in categories:
-    current_category = Category(name=category[0])
+    current_category = Category(name=category[0], user=current_user)
     session.add(current_category)
     session.commit()
 
     for item in category[1]:
         current_item = Item(name=item['name'],
                             description=item['description'],
-                            category=current_category)
+                            category=current_category,
+                            user=current_user)
         session.add(current_item)
         session.commit()
 
